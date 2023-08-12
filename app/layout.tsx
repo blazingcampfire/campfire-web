d3d189d9e5e262b2165eda7225b7e81df766223f
toni-akintola <oakintol@nd.edu>
2023-08-12 01:50:30 -0400

    changed app metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,26 +4,17 @@ import { Lexend_Deca } from "next/font/google"
 
 const lexendDeca = Lexend_Deca({ subsets: ["latin"] })
 export const metadata: Metadata = {
-  title: "Liftoff - AI-Powered Mock Interviews",
+  title: "campfire - feed the fire ",
   openGraph: {
-    title: "Liftoff - AI-Powered Mock Interviews",
-    description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
-    images: [
-      {
-        url: "https://demo.useliftoff.com/opengraph-image",
-      },
-    ],
+    title: "campfire - feed the fire",
+    // description:
+    //   "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
+    // images: [
+    //   {
+    //     url: "https://demo.useliftoff.com/opengraph-image",
+    //   },
+    // ],
   },
-  twitter: {
-    card: "summary_large_image",
-    title: "Liftoff - AI-Powered Mock Interviews",
-    description:
-      "Liftoff is an AI-powered mock interview platform that helps you practice for your next job interview.",
-    images: ["https://demo.useliftoff.com/opengraph-image"],
-    creator: "@tmeyer_me",
-  },
-  metadataBase: new URL("https://demo.useliftoff.com"),
   themeColor: "#FFF",
 };
 
